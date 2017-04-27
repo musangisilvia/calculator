9d51988d6774e0306b0093b03273b61c4b6d4321
musangisilvia <musangisilvia@gmail.com>
2017-04-27 23:33:26 +0300

    a form added that takes two numbers and adds them to diplay the result below the form

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,6 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
 var multiplication = function(number1, number2) {
   return number1 * number2;
 };
@@ -17,5 +14,12 @@ var subtraction = function(number1, number2) {
   return number1 - number2;
 };
 
-var result = subtraction(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
